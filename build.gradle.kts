57811ab805a2153ae28e92c788d8bed3a6994d90
Mbuodile Obiosio <cazewonder@gmail.com>
2020-12-16 19:06:15 +0100

    fully setup cd and gradle dsl

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        uri("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        gradle()
+    }
+}
+
+plugins {
+    id(Dependencies.DETEKT_PLUGIN).version(Dependencies.DETEKT)
+}
+
+dependencies {
+    detektFormatting()
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+tasks.register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    parallel = true
+    setSource(files(projectDir))
+    config.setFrom(files("$rootDir/detekt.yml"))
+    include("**/*.kt")
+    include("**/*.kts")
+    exclude("**/build/**")
+    exclude("**/buildSrc/**")
+    exclude("**/test/**/*.kt")
+    reports {
+        xml.enabled = false
+        html.enabled = false
+        txt.enabled = false
+    }
+}
+
+apply(plugin = "name.remal.check-dependency-updates")
